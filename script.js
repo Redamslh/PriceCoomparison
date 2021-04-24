d2c63936b01c8c423508a54236578fddf8a276e1
Othmane ESSAHABY <othmane.essahaby@gmail.com>
2021-04-24 12:59:41 +0000

    seconde commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 $( document ).ready(function() {
-  let aspectRatioGroup = document.querySelector('#aspectRatio--group .segmentedControl');
+var dataGaz = new Array();
+let aspectRatioGroup = document.querySelector('#aspectRatio--group .segmentedControl');
 let radios = aspectRatioGroup.querySelectorAll('input');
 let i = 1;
 
@@ -81,15 +82,39 @@ aspectRatioGroup.classList.add('useSlidingAnimation');
   var maxgaz = 0.0;
   var maxEssence = 0.0
   var maxExcelum = 0.0
+  function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [day, month, year].join('-');
+}
+console.log(formatDate())
+ var stationrepeat="";
   var station1 = firebase.database().ref('Casablanca/');
+
   station1.on('value',(snapshot)=>{
       snapshot.forEach(function(snapshot1){
           snapshot1.forEach(function(snapshot2){
+
               snapshot2.forEach(function(snapshot3){
                 gaz = snapshot3.val().gasoil
                 spec = snapshot3.val().excellum
                 ess = snapshot3.val().sans_plomb
+                if(snapshot2.key == formatDate()){
+                  if(stationrepeat!=snapshot1.key){
+                    dataGaz.push({category:snapshot1.key,value1:parseFloat(gaz),value2:parseFloat(spec),value3: parseFloat(ess)})
+                  }
+                  stationrepeat = snapshot1.key;
+                  
+                }
+
                 if(parseFloat(gaz) < mingaz){
                   mingaz = parseFloat(gaz);
                   stationgz = snapshot1.key;
@@ -119,6 +144,7 @@ aspectRatioGroup.classList.add('useSlidingAnimation');
           })
           
       })
+      
       console.log("station" + stationgz + " gaz " + mingaz );
       console.log("station" + stationex + " ex " + minExcelum );
       console.log("station" + stationes + " ess " + minEssence );
@@ -141,7 +167,6 @@ aspectRatioGroup.classList.add('useSlidingAnimation');
   var villes = firebase.database().ref('/');
   villes.on('value', (snapshot2) => {
     snapshot2.forEach(function(snapshot3) {
-      
       var opt1 = document.createElement("option");
       opt1.value= snapshot3.key;
       opt1.innerHTML = snapshot3.key;
@@ -186,5 +211,107 @@ aspectRatioGroup.classList.add('useSlidingAnimation');
         })
         
       });
-    
+      function sleep(ms) {
+        return new Promise(
+          resolve => setTimeout(resolve, ms)
+        );
+      }
+      
+      async function delayedGreeting() {
+        await sleep(8000);
+        
+        am4core.ready(function() {
+
+          // Themes begin
+          am4core.useTheme(am4themes_animated);
+          // Themes end
+          
+          
+          
+          var chart = am4core.create('chartdiv', am4charts.XYChart)
+          chart.colors.step = 2;
+          
+          chart.legend = new am4charts.Legend()
+          chart.legend.position = 'top'
+          chart.legend.paddingBottom = 20
+          chart.legend.labels.template.maxWidth = 95
+          
+          var xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+          xAxis.dataFields.category = 'category'
+          xAxis.renderer.cellStartLocation = 0.1
+          xAxis.renderer.cellEndLocation = 0.9
+          xAxis.renderer.grid.template.location = 0;
+          
+          var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+          yAxis.min = 0;
+          
+          function createSeries(value, name,color) {
+              var series = chart.series.push(new am4charts.ColumnSeries())
+              series.dataFields.valueY = value
+              series.dataFields.categoryX = 'category'
+              series.name = name
+              series.fill = color
+              series.events.on("hidden", arrangeColumns);
+              series.events.on("shown", arrangeColumns);
+          
+              var bullet = series.bullets.push(new am4charts.LabelBullet())
+              bullet.interactionsEnabled = false
+              bullet.dy = 30;
+              bullet.label.text = '{valueY}'
+              bullet.label.fill = am4core.color('#ffffff')
+              return series;
+          }
+          
+          chart.data = dataGaz.slice(0,6)
+          
+          
+          createSeries('value1', 'Gasoline','#FFA500');
+          createSeries('value2', 'Excellum','#046307');
+          createSeries('value3', 'Essence','#720500');
+          
+          function arrangeColumns() {
+          
+              var series = chart.series.getIndex(0);
+          
+              var w = 1 - xAxis.renderer.cellStartLocation - (1 - xAxis.renderer.cellEndLocation);
+              if (series.dataItems.length > 1) {
+                  var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
+                  var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
+                  var delta = ((x1 - x0) / chart.series.length) * w;
+                  if (am4core.isNumber(delta)) {
+                      var middle = chart.series.length / 2;
+          
+                      var newIndex = 0;
+                      chart.series.each(function(series) {
+                          if (!series.isHidden && !series.isHiding) {
+                              series.dummyData = newIndex;
+                              newIndex++;
+                          }
+                          else {
+                              series.dummyData = chart.series.indexOf(series);
+                          }
+                      })
+                      var visibleCount = newIndex;
+                      var newMiddle = visibleCount / 2;
+          
+                      chart.series.each(function(series) {
+                          var trueIndex = chart.series.indexOf(series);
+                          var newIndex = series.dummyData;
+          
+                          var dx = (newIndex - trueIndex + middle - newMiddle) * delta
+          
+                          series.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                          series.bulletsContainer.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                      })
+                  }
+              }
+          }
+          
+          }); // end am4core.ready()
+      }
+      
+      delayedGreeting();
+      
+       
 });
+
