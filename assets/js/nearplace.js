11fb5ba64e9ec83bf3b8d9cb38268cee7c5a38da
Redamslh <maslouhireda99@gmail.com>
2021-04-28 02:24:31 +0200

    mslhchanges

---
+++ assets/js/nearplace.js
@@ -0,0 +1,272 @@
+jQuery(document).ready(function($) {
+    var distchosen=10000000 ; 
+	$('.range-slider').each(function(index, el) {
+		$(this).append('<span class="range-number"><span>0</span></span>');
+	});
+	$('.range-slider').on('mousedown touchstart', '.range-number', function(event) {
+		event.preventDefault();
+		var $ele = $(this);
+		var $eleParent = $(this).parents('.range-slider');
+		var eleWid = $ele.innerWidth();
+		var eleOffset = 5;
+		var thisEnd = $eleParent.data('end');
+		var parentWid = $eleParent.innerWidth();
+		console.log(event, $ele, $eleParent);
+		$eleParent.addClass('tap');
+		$eleParent.on('mousemove touchmove', function(event) {
+			event.preventDefault();
+			var leftOff = event.offsetX-(eleWid/2);
+			var leftSpc = (leftOff*100)/$eleParent[0].offsetWidth;
+			var leftText = (leftOff + eleOffset * 2) - eleOffset;
+			if(leftOff <= (parentWid-eleWid-eleOffset) && leftOff >= eleOffset){
+				var temp = parentWid-eleWid-(eleOffset*2);
+				var temp1 = Math.floor(((leftOff-eleOffset)/temp)*thisEnd);
+				$ele.css('left', leftSpc+'%');
+				$ele.find('span').text(temp1);
+			}
+		});
+	});
+	$('.range-slider').on('mouseup touchend', function(event) {
+		event.preventDefault();
+		var $ele = $(this);
+		var $eleParent = $(this).parents('.range-slider');
+		$ele.removeClass('tap');
+		$ele.off('mousemove touchmove');
+        console.log($ele[0].outerText);
+        distchosen=$ele[0].outerText
+
+
+	});
+    var type="essence"; 
+    var lng ; 
+    var lat ; 
+    var chep , near =false ;
+    function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
+      
+      function showPosition(position) {
+        lng =position.coords.longitude 
+          console.log(position.coords.latitude + " " +  position.coords.longitude )
+          lat = position.coords.latitude ;
+
+      }
+      var near,chep =false ; 
+      var i = 0 
+      $('#CheckboxButton1').on('click', function() {
+        if(near){
+          near=false
+        }else{
+          near=true
+        }
+    })
+    $('#CheckboxButton2').on('click', function() {
+      if(chep){
+        chep=false
+      }   
+      else{
+        chep=true
+      }
+  })
+    $('#bgz').on('click', function() {
+        type="gaz"
+       console.log("gaz")
+    })
+    $('#bess').on('click', function() {
+        type="essence"
+        console.log("essence")
+    })
+    $('#bex').on('click', function() {
+      type="ex"
+      console.log("essence")
+  })
+    function removeLoader(){
+        $( "#loader" ).fadeOut(500, function() {
+          // fadeOut complete. Remove the loading div
+     //makes page more lightweight 
+          $('#stationcard').css('display', 'block');
+          cnt = true;
+            document.getElementById("prixgaz").innerHTML = lng
+            getStation()
+            if(chep & !near){
+              if(type=="essence"){
+                document.getElementById("prixgaz").innerHTML = stationes  + " " + minEssence + " DH"
+              }else if(type=="ex"){
+                document.getElementById("prixgaz").innerHTML = stationex + " " +  minExcelum + " DH"
+              }else if(type=="gaz"){
+                document.getElementById("prixgaz").innerHTML = stationgz +" " + mingaz+ " DH"
+                
+              }
+              document.getElementById("box").innerHTML = "Cheapest Station"
+            }else if(near & !chep){
+              document.getElementById("box").innerHTML = "Nearest Station"
+              document.getElementById("prixgaz").innerHTML = nearstation
+            }
+            else if(near && chep ){
+              if(type=="essence"){
+              document.getElementById("prixgaz").innerHTML = nearcheapstationess+ " DH"
+              }else if(type=="ex"){
+                document.getElementById("prixgaz").innerHTML = nearcheapstationex+ " DH"
+              }else if(type=="gaz"){
+                document.getElementById("prixgaz").innerHTML = nearcheapstationgaz + " DH"
+              }
+              document.getElementById("box").innerHTML = "Nearest  & Cheapest Station"
+
+            }
+                 
+    
+    
+  
+
+      });  
+    }
+    var cnt = false;
+    $('#bt3').on('click', function() {
+      if(cnt){
+      $('#stationcard').css('display', 'none');
+      cnt = false;
+      }
+        $('#loader').css('display', 'block');
+        setTimeout(removeLoader, 5000);
+        getLocation();
+         
+    })
+
+    var mingaz,minEssence,minExcelum = 1000.0
+    var maxgaz = 0.0;
+    var maxEssence = 0.0
+    var maxExcelum = 0.0
+    function formatDate() {
+      var d = new Date(),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
+  
+      if (month.length < 2) 
+          month = '0' + month;
+      if (day.length < 2) 
+          day = '0' + day;
+  
+      return [day, month, year].join('-');
+  }
+  var mindist =10000000; 
+
+  var gaz, spec, ess ,cheapestation ,nearstation,nearcheapstationgaz
+    var nearcheappricegaz=100000 ;
+    var nearcheapstationex, nearcheapstationess ;
+    var nearcheappriceex=100000 ;
+    var nearcheappriceess=100000 ;
+    var stationrepeat="";
+    var dataGaz = new Array();
+    var mean=100000000 ; 
+function getStation (){
+   mingaz = 1000.0
+   minEssence = 1000.0
+   minExcelum = 1000.0
+     var station1 = firebase.database().ref('Casablanca/');
+
+     station1.on('value',(snapshot)=>{
+         snapshot.forEach(function(snapshot1){
+             snapshot1.forEach(function(snapshot2){
+   
+                 snapshot2.forEach(function(snapshot3){
+                  if(snapshot2.key != "coordonnee"){
+                   gaz = snapshot3.val().gasoil
+                   spec = snapshot3.val().excellum
+                   ess = snapshot3.val().sans_plomb
+      
+                  }
+                   else{
+                    if(snapshot2.key == formatDate()){
+                      if(stationrepeat!=snapshot1.key){
+                        dataGaz.push({category:snapshot1.key,value1:parseFloat(gaz),value2:parseFloat(spec),value3: parseFloat(ess)})
+                      }
+                      stationrepeat = snapshot1.key;
+                      
+                    }
+    
+                    if(parseFloat(gaz) < mingaz && calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long) < distchosen){
+                      console.log("dazete " + snapshot1.key + " dit : " + distchosen)
+                      mingaz = parseFloat(gaz);
+                      stationgz = snapshot1.key;
+                    }
+                    if(parseFloat(gaz) > maxgaz){
+                      maxgaz = parseFloat(gaz);
+                    }
+                    if(parseFloat(spec) < minExcelum && calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long) < distchosen){
+                      minExcelum = parseFloat(spec);
+                      stationex = snapshot1.key;
+                    }
+                    if(parseFloat(spec) > maxExcelum){
+                      maxExcelum = parseFloat(spec);
+                    }
+                    if(parseFloat(ess) < minEssence && calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long) < distchosen ){
+                      minEssence = parseFloat(ess);
+                      stationes = snapshot1.key;
+                    }
+                    if(parseFloat(ess) > maxEssence){
+                      maxEssence = parseFloat(ess);
+                    }
+if(calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long)<mindist){
+  mindist = calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long)
+  nearstation = snapshot1.key
+}
+meangz = calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long) + 5 * gaz
+meanex = calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long) + 5 * spec
+meaness = calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long) + 5 * ess
+console.log("station " + snapshot1.key + "   chosen " + distchosen + " dist" + calcCrow(lat,lng,snapshot2.val().lat,snapshot2.val().long))
+if(meangz <nearcheappricegaz){
+  nearcheappricegaz = meangz
+  nearcheapstationgaz = snapshot1.key + " " + gaz
+}
+if(meanex <nearcheappriceex){
+  nearcheappriceex = meanex
+  nearcheapstationex = snapshot1.key  + " " + spec
+}
+if(meaness <nearcheappriceess){
+  nearcheappriceess = meaness
+  nearcheapstationess = snapshot1.key + " " + ess
+}
+
+
+                   }
+                   
+                 })
+               
+               
+   
+             })
+             
+         })
+        
+         console.log("nearset station is " + nearstation)
+         console.log("cheapest station is  " + stationgz)
+
+     })
+    }
+
+});
+
+function calcCrow(lat1, lon1, lat2, lon2) 
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+function toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
+ 
---
+++ script.js
@@ -0,0 +1,319 @@
+
+$( document ).ready(function() {
+var dataGaz = new Array();
+let aspectRatioGroup = document.querySelector('#aspectRatio--group .segmentedControl');
+let radios = aspectRatioGroup.querySelectorAll('input');
+let i = 1;
+
+// set CSS Var to number of radios we have
+aspectRatioGroup.style.setProperty('--options',radios.length);
+
+// loop through radio elements
+radios.forEach((input)=>{
+	// store position as data attribute
+	input.setAttribute('data-pos',i);
+	
+	// add click handler to change position
+	input.addEventListener('click',(e)=>{
+		aspectRatioGroup.style.setProperty('--options-active',e.target.getAttribute('data-pos'));
+	});
+	
+	// increment counter
+	i++;
+});
+
+$("#win").click(function() {
+ var station = document.getElementById("newSelect");
+ var prix = document.getElementById("prix");
+ var ville = document.getElementById("villeSelect");
+
+ ville.style.visibility = "hidden";
+ prix.style.visibility = "hidden";
+ station.style.visibility = "hidden";
+
+ document.getElementById("noinfo").innerHTML = "No Data For The Moment"
+
+})
+
+$("#if").click(function() {
+  var station = document.getElementById("newSelect");
+  var prix = document.getElementById("prix");
+  var ville = document.getElementById("villeSelect");
+ 
+  ville.style.visibility = "hidden";
+  prix.style.visibility = "hidden";
+  station.style.visibility = "hidden";
+ 
+  document.getElementById("noinfo").innerHTML = "No Data For The Moment"
+ 
+ })
+$("#tot").click(function() {
+  var station = document.getElementById("newSelect");
+  var prix = document.getElementById("prix");
+  var ville = document.getElementById("villeSelect");
+  var info = document.getElementById("noinfo");
+
+  ville.style.visibility = "visible";
+  prix.style.visibility = "visible";
+  station.style.visibility = "visible";
+
+  document.getElementById("noinfo").innerHTML = ""
+
+ 
+ })
+// add class to enable the sliding pill animation, otherwise it uses a fallback
+aspectRatioGroup.classList.add('useSlidingAnimation');
+  var firebaseConfig = {
+    apiKey: "AIzaSyBdr4MEdpY40GR1r_k85C03T-IM-F2R2n0",
+    authDomain: "gprt-371c9.firebaseapp.com",
+    databaseURL: "https://gprt-371c9-default-rtdb.firebaseio.com",
+    projectId: "gprt-371c9",
+    storageBucket: "gprt-371c9.appspot.com",
+    messagingSenderId: "1069373213441",
+    appId: "1:1069373213441:web:41ae35685e049901e8e900",
+    measurementId: "G-WQP16JMZ52"
+  };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  var mingaz = 1000.0;
+  var minEssence = 1000.0
+  var minExcelum = 1000.0
+  var maxgaz = 0.0;
+  var maxEssence = 0.0
+  var maxExcelum = 0.0
+  function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [day, month, year].join('-');
+}
+console.log(formatDate())
+ var stationrepeat="";
+  var station1 = firebase.database().ref('Casablanca/');
+
+  station1.on('value',(snapshot)=>{
+      snapshot.forEach(function(snapshot1){
+          snapshot1.forEach(function(snapshot2){
+
+              snapshot2.forEach(function(snapshot3){
+                gaz = snapshot3.val().gasoil
+                spec = snapshot3.val().excellum
+                ess = snapshot3.val().sans_plomb
+                if(snapshot2.key == formatDate()){
+                  if(stationrepeat!=snapshot1.key){
+                    dataGaz.push({category:snapshot1.key,value1:parseFloat(gaz),value2:parseFloat(spec),value3: parseFloat(ess)})
+                  }
+                  stationrepeat = snapshot1.key;
+                  
+                }
+
+                if(parseFloat(gaz) < mingaz){
+                  mingaz = parseFloat(gaz);
+                  stationgz = snapshot1.key;
+                }
+                if(parseFloat(gaz) > maxgaz){
+                  maxgaz = parseFloat(gaz);
+                }
+                if(parseFloat(spec) < minExcelum){
+                  minExcelum = parseFloat(spec);
+                  stationex = snapshot1.key;
+                }
+                if(parseFloat(spec) > maxExcelum){
+                  maxExcelum = parseFloat(spec);
+                }
+                if(parseFloat(ess) < minEssence){
+                  minEssence = parseFloat(ess);
+                  stationes = snapshot1.key;
+
+                }
+                if(parseFloat(ess) > maxEssence){
+                  maxEssence = parseFloat(ess);
+                }
+              })
+            
+            
+
+          })
+          
+      })
+      
+      console.log("station" + stationgz + " gaz " + mingaz );
+      console.log("station" + stationex + " ex " + minExcelum );
+      console.log("station" + stationes + " ess " + minEssence );
+      document.getElementById("stgz").innerHTML = "CASABLANCA - " +stationgz;
+      document.getElementById("stes").innerHTML = "CASABLANCA - " +stationes;
+      document.getElementById("stex").innerHTML = "CASABLANCA - " + stationex
+
+      document.getElementById("pgz").innerHTML = mingaz + " DH";
+      document.getElementById("pex").innerHTML = minExcelum + " DH"
+      document.getElementById("pes").innerHTML = minEssence + " DH";
+  })
+  
+
+  newSelect = document.getElementById("newSelect")
+  essence = document.getElementById("ess")
+  gazoil = document.getElementById("gaz")
+  special = document.getElementById("spe")
+ 
+  villeSelect = document.getElementById("villeSelect")
+  var villes = firebase.database().ref('/');
+  villes.on('value', (snapshot2) => {
+    snapshot2.forEach(function(snapshot3) {
+      var opt1 = document.createElement("option");
+      opt1.value= snapshot3.key;
+      opt1.innerHTML = snapshot3.key;
+      villeSelect.appendChild(opt1);
+
+    });
+
+  });
+  var villeSelected;
+  $('#villeSelect').on('change',function(){
+    villeSelected = this.value;
+    newSelect.innerHTML = '';
+    var starCountRef = firebase.database().ref(this.value+'/');
+    starCountRef.on('value', (snapshot) => {
+      snapshot.forEach(function(snapshot1) {
+        if(snapshot1.key != 'Choisissez votre station' ){
+          var opt = document.createElement("option");
+          opt.value= snapshot1.key;
+          opt.innerHTML = snapshot1.key;
+          newSelect.appendChild(opt);
+      }
+ 
+      });
+ 
+    });
+  });
+    $('#newSelect').on('click', function() {
+        var station = firebase.database().ref(villeSelected+'/'+this.value);
+        station.once('value',(snapshot)=>{
+            snapshot.forEach(function(snapshot1){
+              if(snapshot1.key != "coordonnee"){
+                snapshot1.forEach(function(snapshot2){
+                  gaz = snapshot2.val().gasoil
+                  spec = snapshot2.val().excellum
+                  ess = snapshot2.val().sans_plomb
+                  gazoil.innerHTML = '<sup>DH</sup>'+gaz
+                  special.innerHTML = '<sup>DH</sup>'+spec
+                  essence.innerHTML = '<sup>DH</sup>'+ess
+              })
+              }
+            
+                
+            })
+        })
+        
+      });
+      function sleep(ms) {
+        return new Promise(
+          resolve => setTimeout(resolve, ms)
+        );
+      }
+      
+      async function delayedGreeting() {
+        await sleep(8000);
+        
+        am4core.ready(function() {
+
+          // Themes begin
+          am4core.useTheme(am4themes_animated);
+          // Themes end
+          
+          
+          
+          var chart = am4core.create('chartdiv', am4charts.XYChart)
+          chart.colors.step = 2;
+          
+          chart.legend = new am4charts.Legend()
+          chart.legend.position = 'top'
+          chart.legend.paddingBottom = 20
+          chart.legend.labels.template.maxWidth = 95
+          
+          var xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+          xAxis.dataFields.category = 'category'
+          xAxis.renderer.cellStartLocation = 0.1
+          xAxis.renderer.cellEndLocation = 0.9
+          xAxis.renderer.grid.template.location = 0;
+          
+          var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+          yAxis.min = 0;
+          
+          function createSeries(value, name,color) {
+              var series = chart.series.push(new am4charts.ColumnSeries())
+              series.dataFields.valueY = value
+              series.dataFields.categoryX = 'category'
+              series.name = name
+              series.fill = color
+              series.events.on("hidden", arrangeColumns);
+              series.events.on("shown", arrangeColumns);
+          
+              var bullet = series.bullets.push(new am4charts.LabelBullet())
+              bullet.interactionsEnabled = false
+              bullet.dy = 30;
+              bullet.label.text = '{valueY}'
+              bullet.label.fill = am4core.color('#ffffff')
+              return series;
+          }
+          
+          chart.data = dataGaz.slice(0,6)
+          
+          
+          createSeries('value1', 'Gasoline','#ff9b00');
+          createSeries('value2', 'Excellum','#0e9c57');
+          createSeries('value3', 'Essence','#b21919');
+          
+          function arrangeColumns() {
+          
+              var series = chart.series.getIndex(0);
+          
+              var w = 1 - xAxis.renderer.cellStartLocation - (1 - xAxis.renderer.cellEndLocation);
+              if (series.dataItems.length > 1) {
+                  var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
+                  var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
+                  var delta = ((x1 - x0) / chart.series.length) * w;
+                  if (am4core.isNumber(delta)) {
+                      var middle = chart.series.length / 2;
+          
+                      var newIndex = 0;
+                      chart.series.each(function(series) {
+                          if (!series.isHidden && !series.isHiding) {
+                              series.dummyData = newIndex;
+                              newIndex++;
+                          }
+                          else {
+                              series.dummyData = chart.series.indexOf(series);
+                          }
+                      })
+                      var visibleCount = newIndex;
+                      var newMiddle = visibleCount / 2;
+          
+                      chart.series.each(function(series) {
+                          var trueIndex = chart.series.indexOf(series);
+                          var newIndex = series.dummyData;
+          
+                          var dx = (newIndex - trueIndex + middle - newMiddle) * delta
+          
+                          series.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                          series.bulletsContainer.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                      })
+                  }
+              }
+          }
+          
+          }); // end am4core.ready()
+      }
+      
+      delayedGreeting();
+      
+       
+});
+
